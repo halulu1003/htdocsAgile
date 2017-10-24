2b16e799cff4f94cda55c869af6c5876ebabd03e
halulu1003 <youyujia1003@outlook.com>
2017-10-24 15:36:38 +1300

    add for sync

--- bookView.php
+++ bookView.php
@@ -174,7 +174,7 @@ function showBookNotes($student_id,$book_id,$book_name)
 <div class="container-fluid bg-grey">
   <div class="row">
     <div class="col-sm-4">
-      <span class="glyphicon glyphicon-globe logo slideanim"></span>
+      <span class="glyphicon glyphicon-globe logo slideanim"></span> 
     </div>
     <div class="col-sm-8">
 <?php
